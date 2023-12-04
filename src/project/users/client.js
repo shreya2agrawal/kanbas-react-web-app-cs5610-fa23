b635d7c86e037d7753cceba36442cf8753a6a130
jf13-shreya-teammate <938091@cognizant.com>
2023-12-03 19:49:30 -0500

    a6 fixes 4

--- src/project/users/client.js
+++ src/project/users/client.js
@@ -3,9 +3,10 @@ const request = axios.create({
   withCredentials: true,
 });
 
-export const BASE_API = process.env.REACT_APP_API_BASE_URL;
-// export const USERS_API = `${BASE_API}/api/users`;
-export const USERS_API = process.env.REACT_APP_API_URL;
+//export const BASE_API = process.env.REACT_APP_API_BASE_URL;
+export const BASE_API = process.env.REACT_APP_NAPSTER_API_KEY;
+export const USERS_API = `${BASE_API}/api/users`;
+//export const USERS_API = process.env.REACT_APP_API_URL;
 
 export const signin = async (credentials) => {
   const response = await request.post( `${USERS_API}/signin`, credentials );
